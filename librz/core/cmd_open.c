radareorg/radare2

Update cmd_open_map() output format for current map (#17998) ##io

libr/core/cmd_open.c
@@ -6,6 +6,7 @@
 #include "r_util.h"
 #include "r_bin.h"
 #include "r_debug.h"
+#include "r_io.h"
 
 static const char *help_msg_o[] = {
 	"Usage: o","[com- ] [file] ([offset])","",
@@ -422,7 +423,7 @@ static void map_list(RIO *io, int mode, RPrint *print, int fd) {
 		switch (mode) {
 		case 'q':
 			if (fd == -2) {
-				print->cb_printf ("0x%08"PFMT64x"\n", map->itv.addr);
+				print->cb_printf ("0x%08"PFMT64x"\n", r_io_map_get_from (map));
 			} else {
 				print->cb_printf ("%d %d\n", map->fd, map->id);
 			}
@@ -434,16 +435,16 @@ static void map_list(RIO *io, int mode, RPrint *print, int fd) {
 			first = false;
 			print->cb_printf ("{\"map\":%i,\"fd\":%d,\"delta\":%"PFMT64u",\"from\":%"PFMT64u
 					",\"to\":%"PFMT64u",\"perm\":\"%s\",\"name\":\"%s\"}", map->id, map->fd,
-					map->delta, map->itv.addr, r_itv_end (map->itv),
-					r_str_rwx_i (map->perm), (map->name ? map->name : ""));
+					map->delta, r_io_map_get_from (map), r_itv_end (map->itv),
+					r_str_rwx_i (map->perm), r_str_get2 (map->name));
 			break;
 		case 1:
 		case '*':
 		case 'r': {
 			// Need FIFO order here
 			char *om_cmd = r_str_newf ("om %d 0x%08"PFMT64x" 0x%08"PFMT64x" 0x%08"PFMT64x" %s%s%s\n",
-					map->fd, map->itv.addr, map->itv.size, map->delta, r_str_rwx_i(map->perm),
-					map->name ? " " : "", map->name ? map->name : "");
+					map->fd, r_io_map_get_from (map), map->itv.size, map->delta, r_str_rwx_i(map->perm),
+					map->name ? " " : "", r_str_get2 (map->name));
 			if (om_cmd) {
 				om_cmds = r_str_prepend (om_cmds, om_cmd);
 				free (om_cmd);
@@ -453,8 +454,8 @@ static void map_list(RIO *io, int mode, RPrint *print, int fd) {
 		default:
 			print->cb_printf ("%2d fd: %i +0x%08"PFMT64x" 0x%08"PFMT64x
 					" - 0x%08"PFMT64x" %s %s\n", map->id, map->fd,
-					map->delta, map->itv.addr, r_itv_end (map->itv) - 1,
-					r_str_rwx_i (map->perm), (map->name ? map->name : ""));
+					map->delta, r_io_map_get_from (map), r_io_map_get_to (map),
+					r_str_rwx_i (map->perm), r_str_get2 (map->name));
 			break;
 		}
 	}
@@ -570,10 +571,10 @@ static void cmd_open_map(RCore *core, const char *input) {
 	case '.': // "om."
 		map = r_io_map_get (core->io, core->offset);
 		if (map) {
-			core->print->cb_printf ("map: %i fd: %i +0x%"PFMT64x" 0x%"PFMT64x
-				" - 0x%"PFMT64x" ; %s : %s\n", map->id, map->fd,
-				map->delta, map->itv.addr, r_itv_end (map->itv),
-			r_str_rwx_i (map->perm), map->name ? map->name : "");
+			core->print->cb_printf ("%2d fd: %i +0x%08"PFMT64x" 0x%08"PFMT64x
+				" - 0x%08"PFMT64x" %s %s\n", map->id, map->fd,
+				map->delta, r_io_map_get_from (map), r_io_map_get_to (map),
+				r_str_rwx_i (map->perm), r_str_get2 (map->name));
 		}
 		break;
 	case 'r': // "omr"
@@ -1171,7 +1172,7 @@ static bool desc_list_visual_cb(void *user, void *data, ut32 id) {
 				p->cb_printf ("  +0x%"PFMT64x" 0x%"PFMT64x
 					" - 0x%"PFMT64x" : %s : %s : %s\n", map->delta,
 					map->from, map->to, r_str_rwx_i (map->flags), "",
-					map->name ? map->name : "");
+					r_str_get2 (map));
 			}
 		}
 	}

ps1337/rizin

librz/core/cmd_open.c
@@ -6,6 +6,7 @@
 #include "rz_util.h"
 #include "rz_bin.h"
 #include "rz_debug.h"
+#include "rz_io.h"
 
 static const char *help_msg_o[] = {
 	"Usage: o","[com- ] [file] ([offset])","",
@@ -407,7 +408,7 @@ static void map_list(RzIO *io, int mode, RzPrint *print, int fd) {
 		switch (mode) {
 		case 'q':
 			if (fd == -2) {
-				print->cb_printf ("0x%08"PFMT64x"\n", map->itv.addr);
+				print->cb_printf ("0x%08"PFMT64x"\n", rz_io_map_get_from (map));
 			} else {
 				print->cb_printf ("%d %d\n", map->fd, map->id);
 			}
@@ -419,16 +420,16 @@ static void map_list(RzIO *io, int mode, RzPrint *print, int fd) {
 			first = false;
 			print->cb_printf ("{\"map\":%i,\"fd\":%d,\"delta\":%"PFMT64u",\"from\":%"PFMT64u
 					",\"to\":%"PFMT64u",\"perm\":\"%s\",\"name\":\"%s\"}", map->id, map->fd,
-					map->delta, map->itv.addr, rz_itv_end (map->itv),
-					rz_str_rwx_i (map->perm), (map->name ? map->name : ""));
+					map->delta, rz_io_map_get_from (map), rz_itv_end (map->itv),
+					rz_str_rwx_i (map->perm), rz_str_get2 (map->name));
 			break;
 		case 1:
 		case '*':
 		case 'r': {
 			// Need FIFO order here
 			char *om_cmd = rz_str_newf ("om %d 0x%08"PFMT64x" 0x%08"PFMT64x" 0x%08"PFMT64x" %s%s%s\n",
-					map->fd, map->itv.addr, map->itv.size, map->delta, rz_str_rwx_i(map->perm),
-					map->name ? " " : "", map->name ? map->name : "");
+					map->fd, rz_io_map_get_from (map), map->itv.size, map->delta, rz_str_rwx_i(map->perm),
+					map->name ? " " : "", rz_str_get2 (map->name));
 			if (om_cmd) {
 				om_cmds = rz_str_prepend (om_cmds, om_cmd);
 				free (om_cmd);
@@ -438,8 +439,8 @@ static void map_list(RzIO *io, int mode, RzPrint *print, int fd) {
 		default:
 			print->cb_printf ("%2d fd: %i +0x%08"PFMT64x" 0x%08"PFMT64x
 					" - 0x%08"PFMT64x" %s %s\n", map->id, map->fd,
-					map->delta, map->itv.addr, rz_itv_end (map->itv) - 1,
-					rz_str_rwx_i (map->perm), (map->name ? map->name : ""));
+					map->delta, rz_io_map_get_from (map), rz_io_map_get_to (map),
+					rz_str_rwx_i (map->perm), rz_str_get2 (map->name));
 			break;
 		}
 	}
@@ -555,10 +556,10 @@ static void cmd_open_map(RzCore *core, const char *input) {
 	case '.': // "om."
 		map = rz_io_map_get (core->io, core->offset);
 		if (map) {
-			core->print->cb_printf ("map: %i fd: %i +0x%"PFMT64x" 0x%"PFMT64x
-				" - 0x%"PFMT64x" ; %s : %s\n", map->id, map->fd,
-				map->delta, map->itv.addr, rz_itv_end (map->itv),
-			rz_str_rwx_i (map->perm), map->name ? map->name : "");
+			core->print->cb_printf ("%2d fd: %i +0x%08"PFMT64x" 0x%08"PFMT64x
+				" - 0x%08"PFMT64x" %s %s\n", map->id, map->fd,
+				map->delta, rz_io_map_get_from (map), rz_io_map_get_to (map),
+				rz_str_rwx_i (map->perm), rz_str_get2 (map->name));
 		}
 		break;
 	case 'r': // "omr"
@@ -1157,7 +1158,7 @@ static bool desc_list_visual_cb(void *user, void *data, ut32 id) {
 				p->cb_printf ("  +0x%"PFMT64x" 0x%"PFMT64x
 					" - 0x%"PFMT64x" : %s : %s : %s\n", map->delta,
 					map->from, map->to, rz_str_rwx_i (map->flags), "",
-					map->name ? map->name : "");
+					rz_str_get2 (map));
 			}
 		}
 	}